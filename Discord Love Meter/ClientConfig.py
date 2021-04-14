53b35539ce64e8f329466365ee17436f66c88563
Shadi Alostaz <66328589+SilentSerenityy@users.noreply.github.com>
2021-04-14 11:41:24 -0400

    Update ClientConfig.py

--- Discord Love Meter/ClientConfig.py
+++ Discord Love Meter/ClientConfig.py
@@ -11,5 +11,4 @@ async def get_prefix(client,message):
     extras.append(match.group(1))
   return commands.when_mentioned_or(*extras)(client, message)
 
-#client = commands.Bot(command_prefix=(get_prefix),intents = discord.Intents.all())
-client = commands.AutoShardedBot(command_prefix=(get_prefix), description='A Russian Roulette Bot made by Shadi.', case_insensitive=True)# intents=discord.Intents.all())
+client = commands.AutoShardedBot(command_prefix=(get_prefix), description='A Russian Roulette Bot made by Shadi.', case_insensitive=True)
